e472b8f1128a0ee546d3ebec6fa6157299295dcb
EVERYMATRIX\radu.caprescu <radu.caprescu@everymatrix.com>
2016-09-07 14:45:56 +0300

    Added Timeout on Send, modified Version

--- src/Kafka.Basic/ProducerConfig.cs
+++ src/Kafka.Basic/ProducerConfig.cs
@@ -4,9 +4,11 @@
     {
         public const short DefaultAcks = 1;
         public const string DefaultClientId = "DefaultKafkaBasicClientId";
+        public const int DefaultSendTimeout = 1000;
 
         public short Acks { get; set; }
         public string ClientId { get; set; }
+        public int SendTimeout { get; set; }
 
         public static ProducerConfig GetDefaultConfig()
         {
@@ -14,6 +16,7 @@
             {
                 Acks = DefaultAcks,
                 ClientId = DefaultClientId,
+                SendTimeout = DefaultSendTimeout,
             };
         }
 
